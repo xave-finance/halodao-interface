d13f0052f4a29c895e21a80d6ac2cb30cf83beae
James Russell Orola <jrmorola@gmail.com>
2021-04-07 15:02:50 +0800

    Refactor usePoolsSummary main logic from useEffect to useMemo

--- src/halo-hooks/useRewards.ts
+++ src/halo-hooks/useRewards.ts
@@ -1,10 +1,14 @@
 import { HALO_REWARDS_ADDRESS } from '../constants'
 import { useActiveWeb3React } from 'hooks'
 import { useContract } from 'hooks/useContract'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import HALO_REWARDS_ABI from '../constants/haloAbis/Rewards.json'
+import { toCallState, useCallsData, useSingleContractMultipleData } from 'state/multicall/hooks'
+import { formatEther } from 'ethers/lib/utils'
+import { Call } from 'state/multicall/actions'
+import { useBlockNumber } from 'state/application/hooks'
 
-export const useRewards = () => {
+export const useWhitelistedPoolAddresses = () => {
   const { chainId } = useActiveWeb3React()
   const rewardsContract = useContract(chainId ? HALO_REWARDS_ADDRESS[chainId] : undefined, HALO_REWARDS_ABI)
   const [poolAddresses, setPoolAddresses] = useState<string[]>([])
@@ -28,4 +32,106 @@ export const useRewards = () => {
   }, [rewardsContract])
 
   return { poolAddresses }
+}
+
+export const useTotalClaimedHALO = (): number => {
+  const { chainId, account } = useActiveWeb3React()
+  const rewardsContract = useContract(chainId ? HALO_REWARDS_ADDRESS[chainId] : undefined, HALO_REWARDS_ABI)
+
+  const args = useMemo(() => [account ? [account] : []], [account])
+  const data = useSingleContractMultipleData(rewardsContract, 'getTotalRewardsClaimedByUser', args)
+
+  return useMemo<number>(() => {
+    if (data.length) {
+      return data[0].result ? parseFloat(formatEther(data[0].result.toString())) : 0
+    } else {
+      return 0
+    }
+  }, [data])
+}
+
+export const useUnclaimedHALOPerPool = (poolAddresses: string[]): { [poolAddress: string]: number } => {
+  const { chainId, account } = useActiveWeb3React()
+  const rewardsContract = useContract(chainId ? HALO_REWARDS_ADDRESS[chainId] : undefined, HALO_REWARDS_ABI)
+
+  const methodName = 'getUnclaimedPoolRewardsByUserByPool'
+  const callInputs = useMemo(() => poolAddresses.map(address => [address, account]), [poolAddresses, account])
+  const fragment = useMemo(() => rewardsContract?.interface?.getFunction(methodName), [rewardsContract, methodName])
+
+  const calls = useMemo(
+    () =>
+      rewardsContract && fragment && callInputs && callInputs.length > 0
+        ? callInputs.map<Call>(inputs => {
+            return {
+              address: rewardsContract.address,
+              callData: rewardsContract.interface.encodeFunctionData(fragment, inputs)
+            }
+          })
+        : [],
+    [callInputs, rewardsContract, fragment]
+  )
+
+  const results = useCallsData(calls)
+  const latestBlockNumber = useBlockNumber()
+
+  const unclaimedHALOs = useMemo(() => {
+    return results.map(result => toCallState(result, rewardsContract?.interface, fragment, latestBlockNumber))
+  }, [fragment, rewardsContract, results, latestBlockNumber])
+
+  return useMemo(
+    () =>
+      poolAddresses.length > 0
+        ? poolAddresses.reduce<{ [poolAddress: string]: number }>((memo, address, i) => {
+            const unclaimed = unclaimedHALOs[i].result
+            if (unclaimed) {
+              memo[address] = parseFloat(formatEther(unclaimed.toString()))
+            }
+            return memo
+          }, {})
+        : {},
+    [poolAddresses, unclaimedHALOs]
+  )
+}
+
+export const useStakedBPTPerPool = (poolAddresses: string[]): { [poolAddress: string]: number } => {
+  const { chainId, account } = useActiveWeb3React()
+  const rewardsContract = useContract(chainId ? HALO_REWARDS_ADDRESS[chainId] : undefined, HALO_REWARDS_ABI)
+
+  const methodName = 'getDepositedPoolTokenBalanceByUser'
+  const callInputs = useMemo(() => poolAddresses.map(address => [address, account]), [poolAddresses, account])
+  const fragment = useMemo(() => rewardsContract?.interface?.getFunction(methodName), [rewardsContract, methodName])
+
+  const calls = useMemo(
+    () =>
+      rewardsContract && fragment && callInputs && callInputs.length > 0
+        ? callInputs.map<Call>(inputs => {
+            return {
+              address: rewardsContract.address,
+              callData: rewardsContract.interface.encodeFunctionData(fragment, inputs)
+            }
+          })
+        : [],
+    [callInputs, rewardsContract, fragment]
+  )
+
+  const results = useCallsData(calls)
+  const latestBlockNumber = useBlockNumber()
+
+  const stakedBPTs = useMemo(() => {
+    return results.map(result => toCallState(result, rewardsContract?.interface, fragment, latestBlockNumber))
+  }, [fragment, rewardsContract, results, latestBlockNumber])
+
+  return useMemo(
+    () =>
+      poolAddresses.length > 0
+        ? poolAddresses.reduce<{ [poolAddress: string]: number }>((memo, address, i) => {
+            const staked = stakedBPTs[i].result
+            if (staked) {
+              memo[address] = parseFloat(formatEther(staked.toString()))
+            }
+            return memo
+          }, {})
+        : {},
+    [poolAddresses, stakedBPTs]
+  )
 }
