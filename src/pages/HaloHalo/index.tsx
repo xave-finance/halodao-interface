e546e08dcd53be3811d0f96fc3caf48dc2204b66
Barry Lavides <lavidesbarry@gmail.com>
2021-04-19 10:45:28 +0800

    vesting page, dessert pool section

--- src/pages/HaloHalo/index.tsx
+++ src/pages/HaloHalo/index.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react'
-import styled, { ThemeContext } from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
 
 //import { WrapperNoPadding } from '../../components/swap/styleds'
 //import { useDarkModeManager } from '../../state/user/hooks'
-import AppBody from '../AppBody'
+// import AppBody from '../AppBody'
 import HaloChestHeader from './HaloHaloHeader'
 import { Wrapper } from '../../components/swap/styleds'
 
@@ -13,14 +13,15 @@ import HALOHALODepositPanel from './HALOHALODepositPanel'
 import { CardSection, DataCard } from '../../components/earn/styled'
 import { RowBetween } from '../../components/Row'
 import { AutoColumn } from '../../components/Column'
-import { TYPE } from '../../theme'
+// import { TYPE } from '../../theme'
 import { transparentize } from 'polished'
 
 //import { useActiveWeb3React } from '../../hooks'
 
 const PageWrapper = styled(AutoColumn)`
-  max-width: 420px;
+  max-width: 760px;
   width: 100%;
+  display: block;
 `
 
 const VoteCard = styled(DataCard)`
@@ -30,64 +31,98 @@ const VoteCard = styled(DataCard)`
 `
 
 export default function Saave() {
-  const theme = useContext(ThemeContext)
+  // const theme = useContext(ThemeContext)
   //const { account } = useActiveWeb3React()
   //const darkMode = useDarkModeManager()
 
   return (
     <>
       <PageWrapper>
-        <VoteCard>
+        <VoteCard
+          style={{
+            maxWidth: "300px",
+            width: "100%",
+            float: "left"
+          }}
+        >
           <CardSection>
             <AutoColumn gap="md">
-              <RowBetween>
-                <TYPE.white fontWeight={600} color={theme.text1}>
-                  HALO: Make HALO work for you
-                </TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <div>
-                  <TYPE.white fontSize={14} color={theme.text2} style={{ paddingBottom: '10px' }}>
-                    {`Stake your HALO into HALOHALO for ~% APY. No impermanent loss, no loss of governance rights. Continuously compounding.`}
-                  </TYPE.white>
-                </div>
-              </RowBetween>
-              {/*
-
-              // Enable when we have analytics already
-
-              <ExternalLink
-                style={{ color: 'white', textDecoration: 'underline' }}
-                target="_blank"
-                href="https://analytics.sushi.com/bar"
+              <RowBetween
+                style={{
+                  fontFamily: "Open Sans",
+                  fontStyle: "normal",
+                  fontWeight: 800,
+                  lineHeight: "16px",
+                  letterSpacing: "0.2em",
+                  color: "#000000"
+                }}
               >
-           
-                <TYPE.white fontSize={14} color={theme.text1}>
-                  View SushiBar Stats <span style={{ fontSize: '11px' }}>↗</span>
-                </TYPE.white>
-              </ExternalLink>
-
-              
-              {account && (
-                
-                <ExternalLink
-                  style={{ color: 'white', textDecoration: 'underline' }}
-                  target="_blank"
-                  href={'http://analytics.sushi.com/users/' + account}
-                >
-                  <TYPE.white fontSize={14} color={theme.text1}>
-                    View your SushiBar Portfolio <span style={{ fontSize: '11px' }}>↗</span>
-                  </TYPE.white>
-                </ExternalLink>
-              )}
-                 */}
+                VESTING
+              </RowBetween>
+              <RowBetween
+                style={{
+                  fontFamily: "Fredoka One",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontSize: "36px",
+                  lineHeight: "44px",
+                  color: "#471BB2"
+                }}
+              >
+                Dessert Pool
+              </RowBetween>
+              <RowBetween
+                style={{
+                  fontFamily: "Open Sans",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontSize: "16px",
+                  lineHeight: "130%",
+                  color: "#333333"
+                }}
+              >
+                Stake your HALO to earn HALOHALO for ~%APY. Unstake at any time. Continuously compounding.
+              </RowBetween>
+              <RowBetween
+                style={{
+                  fontFamily: "Open Sans",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontSize: "16px",
+                  lineHeight: "130%",
+                  textDecorationLine: "underline",
+                  color: "#518CFF",
+                  cursor: "pointer"
+                }}
+              >
+                Learn about staking
+              </RowBetween>
             </AutoColumn>
           </CardSection>
+          <CardSection
+            style={{
+              background: "blue"
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Parturient id vitae morbi ipsum est maecenas tellus at. Consequat in justo
+          </CardSection>
         </VoteCard>
-        <AppBody>
+        <div
+          style={{
+            width: "440px",
+            float: "right"
+          }}
+        >
           <HaloChestHeader />
-          <Wrapper id="swap-page">
-            <AutoColumn style={{ paddingBottom: '10px' }}>
+          <Wrapper id="swap-page"
+            style={{
+              background: "yellow"
+            }}
+          >
+            <AutoColumn style={{
+              paddingBottom: '10px',
+              background: "green"
+            }}>
               <HaloDepositPanel
                 label={''}
                 disableCurrencySelect={true}
@@ -106,7 +141,7 @@ export default function Saave() {
               />
             </AutoColumn>
           </Wrapper>
-        </AppBody>
+        </div>
       </PageWrapper>
     </>
   )
