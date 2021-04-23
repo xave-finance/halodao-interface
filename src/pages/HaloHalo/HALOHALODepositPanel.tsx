9556d60a33257d0e2505575df883149b2d95844a
Lint Action <lint-action@samuelmeuli.com>
2021-04-23 01:05:11 +0000

    Fix code style issues with ESLint

--- src/pages/HaloHalo/HALOHALODepositPanel.tsx
+++ src/pages/HaloHalo/HALOHALODepositPanel.tsx
@@ -74,7 +74,7 @@ const InputPanel = styled.div<{ hideInput?: boolean }>`
   ${({ theme }) => theme.flexColumnNoWrap}
   position: relative;
   border-radius: ${({ hideInput }) => (hideInput ? '8px' : '20px')};
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   z-index: 1;
   margin: 30px 0 0 0;
 `
