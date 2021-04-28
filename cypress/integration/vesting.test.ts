7c366a8c41e3cd2fa7adc17e566c29f9b9f4043e
Ernest Pascual <eepascual@gmail.com>
2021-04-28 16:13:20 +0800

    fixed cypress tests for vesting

--- cypress/integration/vesting.test.ts
+++ cypress/integration/vesting.test.ts
@@ -6,18 +6,14 @@ describe('Vesting', () => {
   it('displays vesting APY', () => {
     // 1 - Initially APY blank
     cy.contains('0 APY')
-    // 2 - Wait for APY to load
-    cy.wait(1500)
-    // 3 - Check if APY changed
-    cy.get('#haloHaloAPY').should('not.contain', '0 APY')
+    // 2 - Check if APY changed
+    cy.should('not.contain', '0 APY')
   })
 
   it('displays HALO:HALOHALO price', () => {
     // 1 - Initially price is blank
     cy.contains('x0')
-    // 2 - Wait for price to load
-    cy.wait(1500)
-    // 3 - Check for Halohalo price changed
-    cy.get('#haloHaloPrice').should('not.contain', 'x0')
+    // 2 - Check for Halohalo price changed
+    cy.should('not.contain', 'x0')
   })
 })
