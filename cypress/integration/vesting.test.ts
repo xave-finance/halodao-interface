403ee8c1b4cbfd1803ab655802e3d5546108687a
Ernest Pascual <eepascual@gmail.com>
2021-04-28 15:54:39 +0800

    updated cypress tess

--- cypress/integration/vesting.test.ts
+++ cypress/integration/vesting.test.ts
@@ -4,14 +4,20 @@ describe('Vesting', () => {
   })
 
   it('displays vesting APY', () => {
-    // Initially APY blank
-    cy.contains('APY')
-
-    // Then should reload APY
-    // cy.should('not.contain', '% APY')
+    // 1 - Initially APY blank
+    cy.contains('0 APY')
+    // 2 - Wait for APY to load
+    cy.wait(1500)
+    // 3 - Check if APY changed
+    cy.get('#haloHaloAPY').should('not.contain', '0 APY')
   })
 
-  it.skip('displays HALO:HALOHALO price', () => {
-    // @todo
+  it('displays HALO:HALOHALO price', () => {
+    // 1 - Initially price is blank
+    cy.contains('x0')
+    // 2 - Wait for price to load
+    cy.wait(1500)
+    // 3 - Check for Halohalo price changed
+    cy.get('#haloHaloPrice').should('not.contain', 'x0')
   })
 })
