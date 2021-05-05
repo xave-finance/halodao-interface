284523c7b0b678df382cb39aa1e8021bc29357bc
James Russell Orola <jrmorola@gmail.com>
2021-05-05 19:13:05 +0800

    Add: Goerli & BSC Testnet support

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -68,22 +68,42 @@ export const HALO: ChainTokenMap = {
     18,
     'HALO',
     'HALOToken'
+  ),
+  [ChainId.GÖRLI]: new Token(
+    ChainId.GÖRLI,
+    process.env.REACT_APP_HALO_TOKEN_ADDRESS_GOERLI || '',
+    18,
+    'HALO',
+    'HALOToken'
+  ),
+  [ChainId.BSC_TESTNET]: new Token(
+    ChainId.BSC_TESTNET,
+    process.env.REACT_APP_HALO_TOKEN_ADDRESS_BSC_TESTNET || '',
+    18,
+    'HALO',
+    'HALOToken'
   )
 }
 
 // HALO Token Addresses
 export const HALO_TOKEN_ADDRESS: ChainAddressMap = {
-  [ChainId.KOVAN]: process.env.REACT_APP_HALO_TOKEN_ADDRESS_KOVAN || ''
+  [ChainId.KOVAN]: process.env.REACT_APP_HALO_TOKEN_ADDRESS_KOVAN || '',
+  [ChainId.GÖRLI]: process.env.REACT_APP_HALO_TOKEN_ADDRESS_GOERLI || '',
+  [ChainId.BSC_TESTNET]: process.env.REACT_APP_HALO_TOKEN_ADDRESS_BSC_TESTNET || ''
 }
 
 // HALO Rewards Addresses
 export const HALO_REWARDS_ADDRESS: ChainAddressMap = {
-  [ChainId.KOVAN]: process.env.REACT_APP_HALO_REWARDS_ADDRESS_KOVAN || ''
+  [ChainId.KOVAN]: process.env.REACT_APP_HALO_REWARDS_ADDRESS_KOVAN || '',
+  [ChainId.GÖRLI]: process.env.REACT_APP_HALO_REWARDS_ADDRESS_GOERLI || '',
+  [ChainId.BSC_TESTNET]: process.env.REACT_APP_HALO_REWARDS_ADDRESS_BSC_TESTNET || ''
 }
 
 // HALOHALO Addressess
 export const HALOHALO_ADDRESS: ChainAddressMap = {
-  [ChainId.KOVAN]: process.env.REACT_APP_HALOHALO_ADDRESS_KOVAN || ''
+  [ChainId.KOVAN]: process.env.REACT_APP_HALOHALO_ADDRESS_KOVAN || '',
+  [ChainId.GÖRLI]: process.env.REACT_APP_HALOHALO_ADDRESS_GOERLI || '',
+  [ChainId.BSC_TESTNET]: process.env.REACT_APP_HALOHALO_ADDRESS_BSC_TESTNET || ''
 }
 
 // Balancer URLs
@@ -106,7 +126,9 @@ export const COINGECKO_KNOWN_TOKENS: ChainCoinGeckoIdMap = {
     havven: '0x86436BcE20258a6DcfE48C9512d4d49A30C4d8c4',
     aragon: '0x37f03a12241E9FD3658ad6777d289c3fb8512Bc9',
     '0x': '0xccb0F4Cf5D3F97f4a55bb5f5cA321C3ED033f244'
-  }
+  },
+  [ChainId.GÖRLI]: {},
+  [ChainId.BSC_TESTNET]: {}
 }
 
 export const COMMON_CONTRACT_NAMES: { [address: string]: string } = {
