15505243a02d1dfba26d99d96b6758e274633687
James Russell Orola <jrmorola@gmail.com>
2021-06-09 20:09:19 +0800

    Refactor: use constants for hardcoded addresses

--- src/halo-hooks/useSushiPoolInfo.ts
+++ src/halo-hooks/useSushiPoolInfo.ts
@@ -4,6 +4,12 @@ import { useActiveWeb3React } from 'hooks'
 import { getAddress } from '@ethersproject/address'
 import { PoolInfo } from './usePoolInfo'
 
+const BUSD_BNB_LPT_ADDRESS = '0x71e3c96C21D734bFA64D652EA99611Aa64F7D9F6'
+const BUSD_XSGD_LPT_ADDRESS = '0x9A0eeceDA5c0203924484F5467cEE4321cf6A189'
+const BUSD_TOKEN_ADDRESS = '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee'
+const BNB_TOKEN_ADDRESS = '0xae13d989dac2f0debff460ac112a837c89baa7cd'
+const XSGD_TOKEN_ADDRESS = '0x979b00492a1cbf691b1fae867936c01bab0b8c4d'
+
 export const useSushiPoolInfo = (poolAddresses: string[]) => {
   const { chainId } = useActiveWeb3React()
 
@@ -17,7 +23,7 @@ export const useSushiPoolInfo = (poolAddresses: string[]) => {
      * For now we'll hardcode the pool token addresses & other info
      */
     for (const poolAddress of poolAddresses) {
-      if (poolAddress === '0x71e3c96C21D734bFA64D652EA99611Aa64F7D9F6') {
+      if (poolAddress === BUSD_BNB_LPT_ADDRESS) {
         poolsInfo.push({
           pair: 'BUSD/BNB',
           address: getAddress(poolAddress),
@@ -25,33 +31,21 @@ export const useSushiPoolInfo = (poolAddresses: string[]) => {
           liquidity: 0,
           tokens: [
             {
-              address: getAddress('0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee'),
+              address: getAddress(BUSD_TOKEN_ADDRESS),
               balance: 0,
               weightPercentage: 0.5,
-              asToken: new Token(
-                chainId,
-                getAddress('0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee'),
-                18,
-                'BUSD',
-                'Binance USD'
-              )
+              asToken: new Token(chainId, getAddress(BUSD_TOKEN_ADDRESS), 18, 'BUSD', 'Binance USD')
             },
             {
-              address: getAddress('0xae13d989dac2f0debff460ac112a837c89baa7cd'),
+              address: getAddress(BNB_TOKEN_ADDRESS),
               balance: 0,
               weightPercentage: 0.5,
-              asToken: new Token(
-                chainId,
-                getAddress('0xae13d989dac2f0debff460ac112a837c89baa7cd'),
-                18,
-                'BNB',
-                'Wrapped BNB'
-              )
+              asToken: new Token(chainId, getAddress(BNB_TOKEN_ADDRESS), 18, 'BNB', 'Wrapped BNB')
             }
           ],
           asToken: new Token(chainId, getAddress(poolAddress), 18, 'SLP', 'BUSD/BNB SLP')
         })
-      } else if (poolAddress === '0x9A0eeceDA5c0203924484F5467cEE4321cf6A189') {
+      } else if (poolAddress === BUSD_XSGD_LPT_ADDRESS) {
         poolsInfo.push({
           pair: 'BUSD/xSGD',
           address: getAddress(poolAddress),
@@ -59,28 +53,16 @@ export const useSushiPoolInfo = (poolAddresses: string[]) => {
           liquidity: 0,
           tokens: [
             {
-              address: getAddress('0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee'),
+              address: getAddress(BUSD_TOKEN_ADDRESS),
               balance: 0,
               weightPercentage: 0.5,
-              asToken: new Token(
-                chainId,
-                getAddress('0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee'),
-                18,
-                'BUSD',
-                'Binance USD'
-              )
+              asToken: new Token(chainId, getAddress(BUSD_TOKEN_ADDRESS), 18, 'BUSD', 'Binance USD')
             },
             {
-              address: getAddress('0x979b00492a1cbf691b1fae867936c01bab0b8c4d'),
+              address: getAddress(XSGD_TOKEN_ADDRESS),
               balance: 0,
               weightPercentage: 0.5,
-              asToken: new Token(
-                chainId,
-                getAddress('0x979b00492a1cbf691b1fae867936c01bab0b8c4d'),
-                18,
-                'xSGD',
-                'Singapore Dollar'
-              )
+              asToken: new Token(chainId, getAddress(XSGD_TOKEN_ADDRESS), 18, 'xSGD', 'Singapore Dollar')
             }
           ],
           asToken: new Token(chainId, getAddress(poolAddress), 18, 'SLP', 'BUSD/xSGD SLP')
@@ -88,9 +70,9 @@ export const useSushiPoolInfo = (poolAddresses: string[]) => {
       }
     }
 
-    tokenAddresses.push('0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee')
-    tokenAddresses.push('0xae13d989dac2f0debff460ac112a837c89baa7cd')
-    tokenAddresses.push('0x979b00492a1cbf691b1fae867936c01bab0b8c4d')
+    tokenAddresses.push(BUSD_TOKEN_ADDRESS)
+    tokenAddresses.push(BNB_TOKEN_ADDRESS)
+    tokenAddresses.push(XSGD_TOKEN_ADDRESS)
 
     return { poolsInfo, tokenAddresses }
   }, [poolAddresses, chainId])
