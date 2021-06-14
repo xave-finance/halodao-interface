f0859cb47e5fc1b07f562c5a30a2c6e32ac011e2
James Russell Orola <jrmorola@gmail.com>
2021-06-14 16:53:10 +0800

    Increased timeout for integration tests to avoid intermittent fails due to timeout

--- cypress/integration/farm.test.ts
+++ cypress/integration/farm.test.ts
@@ -10,7 +10,7 @@ describe('Farm', () => {
     cy.get('#text-halo-earned').contains('--')
 
     // Then should reload to show the figures
-    cy.get('#text-stakeable-value').should('not.contain', '$ --')
+    cy.get('#text-stakeable-value', { timeout: 30000 }).should('not.contain', '$ --')
     cy.get('#text-staked-value').should('not.contain', '$ --')
     cy.get('#text-halo-earned').should('not.contain', '--')
   })
@@ -20,7 +20,9 @@ describe('Farm', () => {
   })
 
   it('can stake BPT', () => {
-    cy.contains('Manage', { timeout: 30000 }).click()
+    cy.get('body', { timeout: 30000 })
+      .contains('Manage')
+      .click()
     cy.get('#stake-input').type('1')
     cy.get('#stake-button').click()
 
