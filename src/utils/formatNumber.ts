53a9c6d171d944d5b46906e04451883bf89a9407
Lint Action <lint-action@samuelmeuli.com>
2021-07-06 08:34:51 +0000

    Fix code style issues with ESLint

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -41,7 +41,7 @@ export function formatNumber(number: number, key?: NumberFormat) {
 
 export function toFixed(num: number, fixed: number) {
   if (num > 0.00001) {
-    var re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?')
+    const re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?')
     return num.toString().match(re)![0]
   } else {
     return '0.0'
