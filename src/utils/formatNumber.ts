82a7903c6d131cab6c6e694069a12970c2e9c548
Ernest Pascual <eepascual@gmail.com>
2021-07-06 17:17:28 +0800

    Fix: Eslint fix

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -42,7 +42,8 @@ export function formatNumber(number: number, key?: NumberFormat) {
 export function toFixed(num: number, fixed: number) {
   if (num > 0.00001) {
     var re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?')
-    return num.toString().match(re)![0]
+    var matches = num.toString().match(re)
+    return matches ? matches[0] : num.toString()
   } else {
     return '0.0'
   }
