4a27ec3be147e964504861f31fdd9fca3fff99ff
tracy <teresa.arciaga@gmail.com>
2021-08-02 01:41:00 +0800

    Add bridge page in demo.

---
+++ src/components/Tailwind/Panels/SelectedNetworkPanel.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SelectedNetworkPanel = () => {
+  return (
+    <div className="mb-2 w-2/5 flex-col items-center p-4 rounded-card bg-primary-lightest">
+      <div className="flex justify-end">
+        <a href="#" className="text-link-primary text-sm">
+          Change
+        </a>
+      </div>
+      <div>tracy</div>
+      <div>Ethereum</div>
+    </div>
+  )
+}
+
+export default SelectedNetworkPanel
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,32 @@ module.exports = {
       fontFamily: {
         fredoka: ['Fredoka One', 'ui-serif']
       },
+      textColor: {
+        secondary: '#5E5E5E'
+      },
+      borderColor: {
+        primary: {
+          DEFAULT: '#471bb2',
+          dark: '#15006d'
+        }
+      },
       colors: {
         primary: {
           DEFAULT: '#471bb2',
           light: '#e8e4f5',
+          lighter: '#D7CDF0',
           lightest: '#F3F2F8',
           hover: '#15006d',
           disabled: '#9182c6',
           gradientVia: '#5521B6',
           gradientTo: '#2DB7C4'
         },
+        warning: {
+          DEFAULT: '#FA6F44'
+        },
         link: {
-          DEFAULT: '#518cff'
+          DEFAULT: '#518cff',
+          primary: '#471bb2'
         },
         success: {
           DEFAULT: '#1BB233'
