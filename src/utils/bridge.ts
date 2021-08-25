4bff6301a9a9be01dcfdae95ebb10363b8fcc51f
tracy <teresa.arciaga@gmail.com>
2021-08-25 20:09:49 +0800

    Fix shuttleFeeCalculation

--- src/utils/bridge.ts
+++ src/utils/bridge.ts
@@ -1,3 +1,3 @@
 export function calculateShuttleFee(amount: number) {
-  return amount * (1 - Number(process.env.REACT_APP_SHUTTLE_FEE_PERCENTAGE))
+  return amount * Number(process.env.REACT_APP_SHUTTLE_FEE_PERCENTAGE)
 }
