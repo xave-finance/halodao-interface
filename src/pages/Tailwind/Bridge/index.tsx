10856cccfff3ffd2fd39fee65a7b6a48ceb6332d
tracy <teresa.arciaga@gmail.com>
2021-09-06 18:03:57 +0800

    This commit will add a warning for unsupported networks in the bridge.

--- src/pages/Tailwind/Bridge/index.tsx
+++ src/pages/Tailwind/Bridge/index.tsx
@@ -3,7 +3,33 @@ import PageWrapper from 'components/Tailwind/Layout/PageWrapper'
 import PageHeaderLeft from 'components/Tailwind/Layout/PageHeaderLeft'
 import BridgePanel from './BridgePanel'
 import InfoCard from 'components/Tailwind/Cards/InfoCard'
+import PageNotSupported from 'components/Tailwind/Layout/PageNotSupported'
+import { NETWORK_SUPPORTED_FEATURES, NETWORK_LABEL } from '../../../constants/networks'
+import { ChainId } from '@sushiswap/sdk'
+import { useActiveWeb3React } from 'hooks'
 
+const NotSupportedContent = () => {
+  const { chainId } = useActiveWeb3React()
+  const caption = 'Ooops! Sorry this page/feature is not supported in ' + NETWORK_LABEL[chainId as ChainId] + '.'
+  return (
+    <div className="flex items-center bg-white py-6 px-8 border border-primary-hover shadow-md rounded-card">
+      <div className="w-full">
+        <PageNotSupported caption={caption} />
+      </div>
+    </div>
+  )
+}
+const CurrentPanelContent = () => {
+  const { chainId } = useActiveWeb3React()
+  const features = NETWORK_SUPPORTED_FEATURES[chainId as ChainId]
+  let content = <></>
+  if (features?.bridge) {
+    content = <BridgePanel />
+  } else {
+    content = <NotSupportedContent />
+  }
+  return content
+}
 const Bridge = () => {
   return (
     <PageWrapper className="mb-8">
@@ -16,7 +42,7 @@ const Bridge = () => {
         />
       </div>
       <div className="md:float-right md:w-1/2">
-        <BridgePanel />
+        <CurrentPanelContent />
       </div>
       <div className="hidden md:flex items-start md:w-1/2">
         <InfoCard
