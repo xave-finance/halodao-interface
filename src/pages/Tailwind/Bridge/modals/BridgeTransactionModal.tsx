c72f47f649341114f73f956d8fed00e1fdacce26
tracy <teresa.arciaga@gmail.com>
2021-09-06 18:48:42 +0800

    Commented out - estimatedGas

--- src/pages/Tailwind/Bridge/modals/BridgeTransactionModal.tsx
+++ src/pages/Tailwind/Bridge/modals/BridgeTransactionModal.tsx
@@ -24,7 +24,7 @@ interface ConfirmTransactionModalProps {
   state: ConfirmTransactionModalState
   setState: (state: ConfirmTransactionModalState) => void
   successHash: string
-  estimatedGas: string
+  // estimatedGas: string
 }
 
 enum ConfirmTransactionModalState {
@@ -53,8 +53,8 @@ const ConfirmTransactionModal = ({
   wrappedTokenSymbol,
   state,
   setState,
-  successHash,
-  estimatedGas
+  successHash
+  // estimatedGas
 }: ConfirmTransactionModalProps) => {
   const ConfirmContent = () => {
     return (
@@ -180,7 +180,7 @@ const ConfirmTransactionModal = ({
         </div>
 
         <div className="text-center font-semibold text-2xl mb-2">Transaction Confirmed</div>
-        <div className="text-center text-sm text-gray-500 mb-2">
+        <div className="by-secondary-light text-center text-sm text-gray-500 mb-2">
           Your transaction is complete on {NETWORK_LABEL[originChainId]}. Please wait a few minutes for your balance to
           update on {NETWORK_LABEL[destinationChainId]}
         </div>
