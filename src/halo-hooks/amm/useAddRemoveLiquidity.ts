1701b4433bee39c6657db56e9f82d96f8422f758
James Russell Orola <jrmorola@gmail.com>
2021-09-08 07:20:05 +0800

    Removed debug logs

--- src/halo-hooks/amm/useAddRemoveLiquidity.ts
+++ src/halo-hooks/amm/useAddRemoveLiquidity.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react'
 import { useContract } from 'hooks/useContract'
 import CURVE_ABI from 'constants/haloAbis/Curve.json'
 import { BigNumber } from 'ethers'
-import { formatEther, formatUnits } from 'ethers/lib/utils'
+import { formatUnits } from 'ethers/lib/utils'
 import { Token } from '@sushiswap/sdk'
 import { useTransactionAdder } from 'state/transactions/hooks'
 
@@ -12,9 +12,7 @@ export const useAddRemoveLiquidity = (address: string, token0: Token, token1: To
 
   const deposit = useCallback(
     async (amount: BigNumber, deadline: number) => {
-      console.log(`CurveContract.deposit params: `, formatEther(amount), deadline)
       const tx = await CurveContract?.deposit(amount, deadline)
-      console.log(`CurveContract.deposit(${amount.toString()}, ${deadline.toString()}): `, tx)
       addTransaction(tx, {
         summary: `Add Liquidity for ${token0.symbol}/${token1.symbol}`
       })
@@ -26,12 +24,7 @@ export const useAddRemoveLiquidity = (address: string, token0: Token, token1: To
   const viewWithdraw = useCallback(
     async (amount: BigNumber) => {
       const res = await CurveContract?.viewWithdraw(amount)
-      console.log(`CurveContract.viewWithdraw(${formatEther(amount)}): `, res)
-
       const [baseView, quoteView] = res
-      console.log(`Token[0]: `, formatUnits(baseView, token0.decimals))
-      console.log(`Token[1]: `, formatUnits(quoteView, token1.decimals))
-
       return [formatUnits(baseView, token0.decimals), formatUnits(quoteView, token1.decimals)]
     },
     [CurveContract, token0, token1]
@@ -39,9 +32,7 @@ export const useAddRemoveLiquidity = (address: string, token0: Token, token1: To
 
   const withdraw = useCallback(
     async (amount: BigNumber, deadline: number) => {
-      console.log(`CurveContract.withdraw params: `, formatEther(amount), deadline)
       const tx = await CurveContract?.withdraw(amount, deadline)
-      console.log(`CurveContract.withdraw(${amount.toString()}, ${deadline.toString()}): `, tx)
       addTransaction(tx, {
         summary: `Remove Liquidity from ${token0.symbol}/${token1.symbol}`
       })
