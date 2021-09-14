5853f33542c26404e4c88a7c85dccbeb080be905
Ernest Pascual <eepascual@gmail.com>
2021-09-14 11:31:57 +0800

    Fix: Copy change

--- src/pages/Tailwind/Swap/index.tsx
+++ src/pages/Tailwind/Swap/index.tsx
@@ -11,7 +11,7 @@ const Swap = () => {
         <PageHeaderLeft
           subtitle=""
           title="Swap"
-          caption="Swap your local stablecoins at efficient rates"
+          caption=""
           link={{ text: 'Learn about swap', url: 'https://docs.halodao.com/get-started/how-to-earn/how-to-farm' }}
         />
       </div>
@@ -25,10 +25,7 @@ const Swap = () => {
         </div>
       </div>
       <div className="hidden md:flex items-start md:w-1/2">
-        <InfoCard
-          title="Swap Info"
-          description="Deposit your HLP tokens to the Farm to begin earning Rainbow Candies (RNBW). Each RNBW you earn is automatically staked as xRNBW in the Rainbow Pool to begin earning vesting rewards!"
-        />
+        <InfoCard title="Swap Info" description="Swap your local stablecoins at efficient rates." />
       </div>
     </PageWrapper>
   )
