64d8e3d7ee04bcf2f733347d1608057052340de4
psalm <psalm@oostvoort.com>
2022-01-10 09:05:54 +0800

    Re-enable vesting in Arbitrum - URL for deposit and withdraw changed

--- src/pages/HaloHalo/DepositOnUnsupportedNetwork.tsx
+++ src/pages/HaloHalo/DepositOnUnsupportedNetwork.tsx
@@ -15,9 +15,12 @@ const DepositOnUnsupportedNetwork = ({ chainId }: DepositOnUnsupportedNetworkPro
         icon={<ExternalLink />}
         state={PrimaryButtonState.Enabled}
         onClick={() => {
-          if (chainId === ChainId.MATIC || chainId === ChainId.ARBITRUM || chainId === ChainId.ARBITRUM_TESTNET) {
+          if (chainId === ChainId.MATIC) {
             window.location.href =
               'https://app.sushi.com/swap?inputCurrency=0x18e7bdb379928a651f093ef1bc328889b33a560c&outputCurrency=0xc104e54803aba12f7a171a49ddc333da39f47193'
+          } else if (chainId === ChainId.ARBITRUM) {
+            window.location.href =
+              'https://app.sushi.com/swap?inputCurrency=0xA4b7999A1456A481FB0F2fa7E431b9B641A00770&outputCurrency=0x323C11843DEaEa9f13126FE33B86f6C5086DE138'
           }
         }}
       />
--- src/pages/HaloHalo/WithdrawOnUnsupportedNetwork.tsx
+++ src/pages/HaloHalo/WithdrawOnUnsupportedNetwork.tsx
@@ -22,9 +22,12 @@ const WithdrawOnUnsupportedNetwork = ({ chainId }: WithdrawOnUnsupportedNetworkP
         border-2 border-primary
       `}
         onClick={() => {
-          if (chainId === ChainId.MATIC || chainId === ChainId.ARBITRUM || chainId === ChainId.ARBITRUM_TESTNET) {
+          if (chainId === ChainId.MATIC) {
             window.location.href =
               'https://app.sushi.com/swap?inputCurrency=0xc104e54803aba12f7a171a49ddc333da39f47193&outputCurrency=0x18e7bdb379928a651f093ef1bc328889b33a560c'
+          } else if (chainId === ChainId.ARBITRUM) {
+            window.location.href =
+              'https://app.sushi.com/swap?inputCurrency=0x323C11843DEaEa9f13126FE33B86f6C5086DE138&outputCurrency=0xA4b7999A1456A481FB0F2fa7E431b9B641A00770'
           }
         }}
       >
