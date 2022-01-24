22f995be73664dc3ff19d03697b72cbf6d0f8529
psalm <psalm@oostvoort.com>
2022-01-24 08:45:25 +0800

    Cypress testing arbitrum wordChanged to kovan

--- cypress/integration/vesting-availability-in-arbitrum.test.ts
+++ cypress/integration/vesting-availability-in-arbitrum.test.ts
@@ -1,8 +1,8 @@
-describe('Test Availability of vesting in arbitrum network', function() {
+describe('Test Availability of vesting in Kovan network', function() {
   it('Visit vesting page of Halo Dao app', function() {
     cy.visit('/vesting')
   })
-  it('Recognizing vesting in Arbitrum Network', function() {
+  it('Recognizing vesting in Kovan Network', function() {
     cy.get('body').then(body => {
       expect(false).to.equal(body.find('#unsupported_vesting_component').length > 0)
     })
