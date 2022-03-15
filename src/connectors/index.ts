342024a7d4564a92e2d88eaf9aa81dd0642b7f35
Geonell Montehermoso <geonell@oostvoort.com>
2022-03-15 11:55:34 +0800

    Enable Wrong Network handler

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -44,13 +44,13 @@ export const injected = new InjectedConnector({
     4, // rinkeby
     5, // goreli
     42, // kovan
-    250, // fantom
-    4002, // fantom testnet
+    // 250, // fantom
+    // 4002, // fantom testnet
     137, // matic
     80001, // matic testnet
     100, // xdai
-    56, // binance smart chain
-    97, // binance smart chain testnet
+    // 56, // binance smart chain
+    // 97, // binance smart chain testnet
     1287, // moonbase
     42161, // arbitrum
     421611 // arbitrum testnet
