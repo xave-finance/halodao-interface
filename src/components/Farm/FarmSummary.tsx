c0008c405077a39457d1f219d832022165ac0729
Geonell Montehermoso <geonell@oostvoort.com>
2022-03-30 15:43:20 +0800

    Display Farm TVL

--- src/components/Farm/FarmSummary.tsx
+++ src/components/Farm/FarmSummary.tsx
@@ -8,15 +8,16 @@ import StakeCard from '../Tailwind/Cards/StakeCard'
 interface FarmSummaryProps {
   poolsInfo: PoolInfo[]
   tokenPrice: TokenPrice
+  farmTVL: string
 }
 
-const FarmSummary = ({ poolsInfo, tokenPrice }: FarmSummaryProps) => {
+const FarmSummary = ({ poolsInfo, tokenPrice, farmTVL }: FarmSummaryProps) => {
   const summary = useFarmSummary(poolsInfo, tokenPrice)
   const { t } = useTranslation()
 
   return (
     <div className="w-full flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 pb-2">
-      <StakeCard title={t('totalPoolValue')} value="5000.03" />
+      <StakeCard title={t('totalPoolValue')} value={farmTVL} />
       <StakeCard title={t('poolSummaryHaloEarned')} value={summary.haloEarned} />
       <StakeCard title={t('poolSummaryStaked')} value={summary.stakedValue} />
       <StakeCard title={t('poolSummaryStakeable')} value={summary.stakeableValue} />
