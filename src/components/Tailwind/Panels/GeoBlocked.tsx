b7268f127f94c246c360e0473876b8eda38f8a00
Lint Action <lint-action@samuelmeuli.com>
2022-06-23 08:43:08 +0000

    Fix code style issues with ESLint

--- src/components/Tailwind/Panels/GeoBlocked.tsx
+++ src/components/Tailwind/Panels/GeoBlocked.tsx
@@ -7,25 +7,27 @@ interface GeoBlockedProps {
   country: GeofenceCountry
 }
 
-const GeoBlocked = ({children, country}: GeoBlockedProps) => {
-    const { loading, rejected } = useGeofence(country)
-    if (loading) return (
-        <>
-            <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-                <div className="w-full">
-                    <PageWarning caption={`Loading...`} />
-                </div>
-            </div>
-        </>
+const GeoBlocked = ({ children, country }: GeoBlockedProps) => {
+  const { loading, rejected } = useGeofence(country)
+  if (loading)
+    return (
+      <>
+        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+          <div className="w-full">
+            <PageWarning caption={`Loading...`} />
+          </div>
+        </div>
+      </>
     )
-    else if (!loading && rejected) return (
-        <>
-             <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-                <div className="w-full">
-                    <PageWarning caption={`Oops! Content is blocked on your location.`} />
-                </div>
-            </div>
-        </>
+  else if (!loading && rejected)
+    return (
+      <>
+        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+          <div className="w-full">
+            <PageWarning caption={`Oops! Content is blocked on your location.`} />
+          </div>
+        </div>
+      </>
     )
 
   return <>{children}</>
