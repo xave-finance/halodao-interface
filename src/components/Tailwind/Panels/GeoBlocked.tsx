9db19b39224b48140e756838f359d123df3574c2
Lint Action <lint-action@samuelmeuli.com>
2022-06-23 06:50:22 +0000

    Fix code style issues with ESLint

--- src/components/Tailwind/Panels/GeoBlocked.tsx
+++ src/components/Tailwind/Panels/GeoBlocked.tsx
@@ -3,36 +3,34 @@ import PageWarning from '../Layout/PageWarning'
 import { GeofenceCountry, useGeofence } from '../../../halo-hooks/useGeofence'
 
 interface GeoBlockedProps {
-    children: React.ReactNode,
-    country: GeofenceCountry
+  children: React.ReactNode
+  country: GeofenceCountry
 }
 
-const GeoBlocked = ({children, country}: GeoBlockedProps) => {
-    const { loading, rejected } = useGeofence(country)
-    if (loading) return (
-        <>
-            <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-                <div className="w-full">
-                    <PageWarning caption={`loading`} />
-                </div>
-            </div>
-        </>
+const GeoBlocked = ({ children, country }: GeoBlockedProps) => {
+  const { loading, rejected } = useGeofence(country)
+  if (loading)
+    return (
+      <>
+        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+          <div className="w-full">
+            <PageWarning caption={`loading`} />
+          </div>
+        </div>
+      </>
     )
-    else if (!loading && rejected) return (
-        <>
-             <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-                <div className="w-full">
-                    <PageWarning caption={`Oops! Content is blocked on your location.`} />
-                </div>
-            </div>
-        </>
+  else if (!loading && rejected)
+    return (
+      <>
+        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+          <div className="w-full">
+            <PageWarning caption={`Oops! Content is blocked on your location.`} />
+          </div>
+        </div>
+      </>
     )
 
-    return (
-        <>
-            {children}
-        </>
-    )
+  return <>{children}</>
 }
 
 export default GeoBlocked
