b910a3b84956ab2ade3e97398b1905bb17c38bf7
Lint Action <lint-action@samuelmeuli.com>
2022-06-24 05:35:43 +0000

    Fix code style issues with ESLint

--- src/components/Tailwind/Panels/GeoBlocked.tsx
+++ src/components/Tailwind/Panels/GeoBlocked.tsx
@@ -3,30 +3,32 @@ import PageWarning from '../Layout/PageWarning'
 import { GeofenceCountry, useGeofence } from '../../../halo-hooks/useGeofence'
 
 interface GeoBlockedProps {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-const BLOCKED_COUNTRIES = [ GeofenceCountry.SINGAPORE ]
+const BLOCKED_COUNTRIES = [GeofenceCountry.SINGAPORE]
 
 const GeoBlocked = ({ children }: GeoBlockedProps) => {
-    const { loading, rejected } = useGeofence(BLOCKED_COUNTRIES)
-    
-    if (loading) return (
-        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-            <div className="w-full">
-                <PageWarning caption={`Loading...`} />
-            </div>
+  const { loading, rejected } = useGeofence(BLOCKED_COUNTRIES)
+
+  if (loading)
+    return (
+      <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+        <div className="w-full">
+          <PageWarning caption={`Loading...`} />
         </div>
+      </div>
     )
-    else if (!loading && rejected) return (
-        <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
-            <div className="w-full">
-                <PageWarning caption={`You are not allowed to access Xave Finance due to the restricted jurisdiction.`} />
-            </div>
+  else if (!loading && rejected)
+    return (
+      <div className="flex items-center border border-primary-hover shadow-md rounded-card bg-white m-20 pt-10 pb-10">
+        <div className="w-full">
+          <PageWarning caption={`You are not allowed to access Xave Finance due to the restricted jurisdiction.`} />
         </div>
+      </div>
     )
 
-    return <>{children}</>
+  return <>{children}</>
 }
 
 export default GeoBlocked
